7df180e676d6af3e2291b03deb5a56a243676e8e
Yama Roni <20947540+yamaroni@users.noreply.github.com>
2026-02-19 21:44:06 +0700

    Implement delete all functionality and enhance todo display logic

--- js/script.js
+++ js/script.js
@@ -18,11 +18,37 @@ function addTodo() {
 
         // Add the new todo to the array
         todos.push(newTodo);
+
+        // Clear the input fields after adding the todo
+        todoInput.value = '';
+        todoDate.value = '';
+
+        // Display the updated list of todos
+        displayTodos();
     }
 }
 
-function displayTodos() { }
+/// Function to render todo items to the DOM
+function displayTodos() {
+    const todoList = document.getElementById('todo-list');
 
-function deleteTodo() { }
+    // Clear existing list
+    todoList.innerHTML = '';
+
+    // Render each todo item
+    todos.forEach((todo, _) => {
+        todoList.innerHTML += `
+        <li>
+            <p class="text-2xl">${todo.text} <span class="text-sm text-gray-500">(${todo.date})</span></p>
+            <hr />
+        </li>`;
+    });
+}
+
+function deleteAllTodo() {
+    // Clear the todos array
+    todos = [];
+    displayTodos();
+}
 
 function filterTodos() { }
